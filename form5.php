c81437f901e9c7b066e89c734b63ff6964b87c19
blindicide-mainpc <y1rs.yrs@gmail.com>
2023-10-06 15:37:01 +0300

    php

---
+++ form5.php
@@ -0,0 +1,12 @@
+﻿<?php
+echo $_FILES['filename']['type'] . '<br>';
+
+if(move_uploaded_file($_FILES['filename']['tmp_name'],
+                'my_files/' . $_FILES['filename']['name'])){
+    echo "Документ под названием" . $_FILES['filename']['name']
+                        . ' загружен на сервер';
+                }
+    else{
+        echo "Загрузка докумета не удалась. Обратитесь к администратору сети.";
+}
+?>
