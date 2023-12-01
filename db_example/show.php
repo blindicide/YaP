b18b8d5e71a6f7260d3b689213b87763c78e5f40
blindicide-w <y1rs.yrs@gmail.com>
2023-12-01 15:28:29 +0300

    хз

--- db_example/show.php
+++ db_example/show.php
@@ -1,18 +1,19 @@
-
-<meta charset="utf-8">
 <?php
-{
-	$conn = new PDO("mysql:host=localhost;dbname=j19458505_univ.mydb", "j19458505", "23112004");
-	$sql = "SELECT * FROM Users";
-	$result = $conn->query($sql);
-	echo "<table><tr><th>Id</th><th>Name</th><th>Age</th><th>Salary</th></tr>";
-	while ($row = $result->fetch()){
-		echo "<tr>";
-		echo "<td>".$row["id"]."</td>";
-		echo "<td>".$row["name"]."</td>";
-		echo "<td>".$row["age"]."</td>";
-		echo "<td>".$row["salary"]."</td>";
-		echo "</tr>";
+	try {
+		$conn = new PDO("mysql:host=localhost;dbname=j19458505_univ.mydb", "j19458505", "23112004");
+		$sql = "SELECT * FROM Users";
+		$result = $conn->query($sql);
+		echo "<table><tr><th>Id</th><th>Name</th><th>Age</th><th>Salary</th></tr>";
+		while ($row = $result->fetch()){
+			echo "<tr>";
+				echo "<td>".$row["id"]."</td>";
+				echo "<td>".$row["name"]."</td>";
+				echo "<td>".$row["age"]."</td>";
+				echo "<td>".$row["salary"]."</td>";
+			echo "</tr>";
+		}
+		echo "</table>";
 	}
-	echo "</table>";
-}
+		catch (PDOException $e) {
+		echo "Database error: " . $e->getMessage();
+		}
